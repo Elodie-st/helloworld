d4241da580efb69e618a00a1df5e0efa26d07c11
Elodie-st <elodie.somerlinck@student.heaj.be>
2025-10-03 11:53:59 +0200

    popup

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,24 @@
-console.info('Hello world');
+"use strict";
+let btn = document.getElementById("btn");
+let vue = document.getElementById("vue");
+let fermer = document.getElementById("btnFermer")
+
+btn.addEventListener("click", function() {
+  
+
+    if (vue.style.display === "none") {
+        vue.style.display = "block" 
+         
+    }else{
+        vue.style.display = "none"
+        btn.style.boxShadow = "3px 5px 0.5em #ffffff"
+     
+    }
+});
+
+fermer.addEventListener("click", function() {
+    if (vue.style.display === "block") {
+        vue.style.display = "none" 
+         
+    }
+});
